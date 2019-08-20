235310873de7afcd426d2244550487cae929e62f
merve.silk <secondstarfrmtherite@gmail.com>
2019-08-20 23:20:55 +0100

    File updated, includes original js

--- triggerTweet.js
+++ triggerTweet.js
@@ -8,50 +8,25 @@ var stream = T.stream("user");
 
 stream.on("follow", followed);
 
-// function followed(eventMsg) {
-//   var name = eventMsg.source.name;
-//   var screenName = eventMsg.source.screen_name;
-//   tweetIt('I was followed by: ' + name + ' ' + screenName)
-// }
-//
-// function tweetIt(txt) {
-//
-//   var tweet = {
-//     status: txt
-//   }
-//
-//   T.post("statuses/update", tweet, postTweet);
-//
-//   function postTweet(error, data, response) {
-//     if (error) {
-//       console.log(error);
-//     } else {
-//       console.log("Hurrah!");
-//     }
-//   };
-// }
-
-
-
-function followed(eventMsg){
-  console.log('Follow event');
+function followed(eventMsg) {
   var name = eventMsg.source.name;
   var screenName = eventMsg.source.screen_name;
-  tweetMessage('@' + screenName + " Thank you for following me!")
-} // callback function for follow event
+  tweetIt('I was followed by: ' + name + ' ' + screenName)
+}
 
-function tweetMessage(txt){
+function tweetIt(txt) {
+
   var tweet = {
     status: txt
   }
 
-T.post('statuses/update', tweet, tweeted)
+  T.post("statuses/update", tweet, postTweet);
 
-function tweeted(err, data, response) {
-  if(err) {
-    console.log(err);
-  } else {
-    console.log("Voila It worked!");
-}
-}
+  function postTweet(error, data, response) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Hurrah!");
+    }
+  };
 }
